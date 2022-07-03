cec631e78c824d1d72c2ebec61b39b87a618e6df
Aureliano2022 <2383659156@qq.com>
2022-07-03 18:46:05 +0800

    Site updated: 2022-07-03 18:46:04

---
+++ js/funny title.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        document.title = '来看小贺的个站！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+        document.title = '哈哈入股不亏！' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
